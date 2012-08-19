f7cf3807d3501f243da41acc00bd99b290c37020
Jay <jaykzone@gmail.com>
2012-08-19 14:32:58 +0100

    Second commit

--- src/testPackage/GitHubTestClass.java
+++ src/testPackage/GitHubTestClass.java
@@ -3,5 +3,6 @@ package testPackage;
 public class GitHubTestClass {
 	public static void main(String[] args) {
 		System.out.println("Integration with GitHub completed.");
+		System.out.println("It's Done!");
 	}
 }
